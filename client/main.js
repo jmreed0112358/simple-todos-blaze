3e3f2017cf70d960193e0174ab414fc4a12a5e1a
Jeremy M. Reed <reeje76@gmail.com>
2016-10-16 12:58:04 -0600

    Add user signin and signup.
    Add username field to task listing.

--- client/main.js
+++ client/main.js
@@ -1,3 +1,4 @@
 'use strict';
 
+import '../imports/startup/accounts-config.js';
 import '../imports/ui/body.js';
--- imports/ui/body.js
+++ imports/ui/body.js
@@ -39,6 +39,8 @@ Template.body.events({
     Tasks.insert({
       text,
       createdAt: new Date(),
+      owner: Meteor.userId(),
+      username: Meteor.user().username
     });
 
     // Clear form
