78a91780b44b4afbdf5ce51229162dbc898d6d88
Jeremy M. Reed <reeje76@gmail.com>
2016-10-16 18:51:34 -0600

    Security with methods step.

--- imports/ui/task.js
+++ imports/ui/task.js
@@ -6,11 +6,9 @@ import './task.html';
 
 Template.task.events({
   'click .toggle-checked': function() {
-    Tasks.update(this._id, {
-      $set: { checked: ! this.checked }
-    });
+    Meteor.call('tasks.setChecked', this._id, !this.checked);
   },
   'click .delete': function() {
-    Tasks.remove(this._id);
+    Meteor.call('tasks.remove', this._id);
   },
 });
